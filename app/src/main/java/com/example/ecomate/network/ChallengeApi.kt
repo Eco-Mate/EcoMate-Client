e7bbfd73be6c65a1197b287b849a7882746fd784
leehandsub <gustjq7027@naver.com>
2023-07-25 23:01:38 +0900

    feat : 챌린지 post 기능

--- app/src/main/java/com/example/ecomate/network/ChallengeApi.kt
+++ app/src/main/java/com/example/ecomate/network/ChallengeApi.kt
@@ -1,9 +1,12 @@
 package com.example.ecomate.network
 
-import com.example.ecomate.model.ChallengeListBody
+import com.example.ecomate.model.Challenge
 import com.example.ecomate.model.ChallengeDetailBody
+import com.example.ecomate.model.ChallengeListBody
 import com.example.ecomate.model.MYChallengeListBody
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ChallengeApi {
@@ -12,6 +15,9 @@ interface ChallengeApi {
 
     @GET("v1/challenges/{challengeId}")
     suspend fun getChallenge(@Path("challengeId") challengeId: Int): ChallengeDetailBody
+
+    @POST("v1/challenges/form")
+    suspend fun postChallenge(@Body challenge: Challenge)
 
     @GET("v1/myChallenges/user/{userId}/proceeding")
     suspend fun getAllProceedingChallenge(@Path("userId") challengeId: Int): MYChallengeListBody
