b0e24590d31377ff863a5a932ad164286164f6c4
HJTN <zintnm21@gmail.com>
2023-09-05 15:27:48 +0900

    feat: 마이프로필 예외 처리

--- app/src/main/java/com/example/ecomate/viewmodel/MyProfileViewModel.kt
+++ app/src/main/java/com/example/ecomate/viewmodel/MyProfileViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.ecomate.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,6 +11,8 @@ import com.example.ecomate.model.MyDetailChallenge
 import com.example.ecomate.model.ProfileInfo
 import com.example.ecomate.network.RetrofitUtil
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.lang.Exception
 
 class MyProfileViewModel : ViewModel() {
     private val _profileInfo = MutableLiveData<ProfileInfo>()
@@ -41,13 +44,23 @@ class MyProfileViewModel : ViewModel() {
     }
     fun getMyBoards() {
         viewModelScope.launch {
-            _myBoards.value = RetrofitUtil.boardApi.getMyBoards().response["boardDtoList"]
+            try {
+                _myBoards.value = RetrofitUtil.boardApi.getMyBoards().response["boardDtoList"]
+            } catch (e: Exception) {
+                Log.d("ServerError",e.toString())
+                _myBoards.value = mutableListOf()
+            }
         }
     }
 
     fun getMyAllChallenge() {
         viewModelScope.launch {
-            _myAllChallenges.value = RetrofitUtil.challengeApi.getMyAllChallenge().response
+            try {
+                _myAllChallenges.value = RetrofitUtil.challengeApi.getMyAllChallenge().response
+            } catch (e: Exception) {
+                Log.d("ServerError",e.toString())
+                _myAllChallenges.value = mutableListOf()
+            }
         }
     }
 
